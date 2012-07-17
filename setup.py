10ff23d0f799cd738f2adf185cf084cdcff4e47d
James Pic <jamespic@gmail.com>
2012-07-17 14:40:09 +0200

    Package creation

---
+++ setup.py
@@ -0,0 +1,92 @@
+import os
+import sys
+
+from setuptools import setup, find_packages, Command
+
+
+# Utility function to read the README file.
+# Used for the long_description. It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class RunTests(Command):
+    description = "Run the django test suite from the testproj dir."
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        this_dir = os.getcwd()
+        testproj_dir = os.path.join(this_dir, "test_project")
+        os.chdir(testproj_dir)
+        sys.path.append(testproj_dir)
+        from django.core.management import execute_manager
+        os.environ["DJANGO_SETTINGS_MODULE"] = 'test_project.settings'
+        settings_file = os.environ["DJANGO_SETTINGS_MODULE"]
+        settings_mod = __import__(settings_file, {}, {}, [''])
+        execute_manager(settings_mod, argv=[
+            __file__, "test", "eav_forms"])
+        os.chdir(this_dir)
+
+if 'sdist' in sys.argv:
+    # clear compiled mo files before building the distribution
+    walk = os.walk(os.path.join(os.getcwd(), 'eav_forms/locale'))
+    for dirpath, dirnames, filenames in walk:
+        if not filenames:
+            continue
+
+        if 'django.mo' in filenames:
+            os.unlink(os.path.join(dirpath, 'django.mo'))
+            print 'unlink', os.path.join(dirpath, 'django.mo')
+else:
+    # if django is there, compile the po files to mo,
+    try:
+        import django
+    except ImportError:
+        pass
+    else:
+        dir = os.getcwd()
+        os.chdir(os.path.join(dir, 'eav_forms'))
+        os.system('django-admin.py compilemessages')
+        os.chdir(dir)
+
+setup(
+    name='django-eav-forms',
+    version='0.0.1',
+    description='Create forms via drag n drop',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='http://django-eav-forms.rtfd.org',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    long_description=read('README.rst'),
+    license='MIT',
+    keywords='django eav forms',
+    cmdclass={'test': RunTests},
+    install_requires=[
+        'django',
+        'eav_django',
+        'django_crispy_forms',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
